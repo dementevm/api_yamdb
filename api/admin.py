90b5b212763854999e8d58fe08da806d4f5e0ba3
Sergey Vdovin <vdserg@yandex.ru>
2020-08-08 16:08:25 +0500

    Comment, Review commit

---
+++ api/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from api.models import Comment, Review
+
+
+admin.site.register(Comment)
+admin.site.register(Review)
+
---
+++ api/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
+
+from api.views import CommentViewSet, ReviewViewSet
+
+v1_router = DefaultRouter()
+v1_router.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    viewset=ReviewViewSet, basename='reviews'
+)
+v1_router.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    viewset=CommentViewSet, basename='comments'
+)
+
+
+urlpatterns = [
+    path('v1/', include(v1_router.urls)),
+    path('v1/token', TokenObtainPairView.as_view()),
+    path('v1/token/refresh', TokenRefreshView.as_view())
+]
