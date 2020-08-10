2e23971c5770e9acaf737054cf42f1cebfc5a508
Ольга <oe.goncharova@gmail.com>
2020-08-10 08:03:18 +0300

    My second try part 2

--- api/permissions.py
+++ api/permissions.py
@@ -1,8 +1,8 @@
-from rest_framework.permissions import SAFE_METHODS, IsAdminUser
+from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
-class IsAdminUserOrReadOnly(IsAdminUser):
-
+class IsAdminUserOrReadOnly(BasePermission):
     def has_permission(self, request, view):
-        is_admin = super().has_permission(request, view)
-        return request.method in SAFE_METHODS or is_admin
+        if request.method in SAFE_METHODS:
+            return True
+        return request.user.is_superuser
